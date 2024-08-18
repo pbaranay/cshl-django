2c1c0902b080368dda909c3452b7b383fda7441b
Paul Baranay <pbaranay@gmail.com>
2024-08-18 23:37:30 +0000

    Add a basic homepage.
    Notice how this requires adding our web app to INSTALLED_APPS, and
    that we put a templates directory under the web directory as well.

--- bearlab/bearlab/urls.py
+++ bearlab/bearlab/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from web import views as web_views
+
+
 urlpatterns = [
+    path('', web_views.homepage),
     path('admin/', admin.site.urls),
 ]
