0f17fc7f2a464f09f81fa4daae12a7e13f9e9145
Paul Baranay <pbaranay@gmail.com>
2024-08-19 00:14:27 +0000

    Demonstrate using context in views and templates.

--- bearlab/web/views.py
+++ bearlab/web/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    return render(request, 'homepage.html', {
+        'lab_name': 'Baranay',
+        'current_time': timezone.now(),
+    })
