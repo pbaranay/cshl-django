13daa982dbd38d145a78ce7a54e8ed69249d6503
Paul Baranay <pbaranay@gmail.com>
2024-08-19 00:18:39 +0000

    Create our staff model.
    The migration can be generated with
    `python manage.py makemigrations lab` (but once generated, it should
    be added to your source control).

--- bearlab/lab/models.py
+++ bearlab/lab/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Staff(models.Model):
+    # id is added implicitly
+    name = models.CharField(max_length=127)
+    position = models.CharField(max_length=127)
+    date_joined = models.DateField()
+    # blank makes it optional in forms and validation;
+    # null makes it optional in the database
+    date_left = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
